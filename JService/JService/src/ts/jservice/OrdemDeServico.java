092baf19e9e3630b02c26f88cdba8bab1c14a585
ShirleiOliveira <luolivver@hotmail.com>
2014-04-10 10:23:20 -0300

    Painel de ordem de serviço

--- JService/JService/src/ts/jservice/OrdemDeServico.java
+++ JService/JService/src/ts/jservice/OrdemDeServico.java
@@ -1,27 +1,88 @@
 package ts.jservice;
+import javax.swing.JPanel;
+import javax.swing.JLabel;
+import javax.swing.JTextField;
+import java.awt.GridBagLayout;
+import java.awt.GridBagConstraints;
+import java.awt.Insets;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
+import javax.swing.JButton;
+import java.awt.Font;
+import javax.swing.JTextArea;
+import javax.swing.AbstractAction;
+import javax.swing.Action;
 
-import java.util.Date;
 
-public class OrdemDeServico {
+public class OrdemDeServico extends JPanel {
+	private JTextField tfEquipamento;
+	private JTextField tfCliente;
+	private final Action actionGravarOrdem = new SwingAction();
+	private final Action actionCancelar = new SwingAction_1();
 
-	public OrdemDeServico(int i, int j, String string) {
-		// TODO Auto-generated constructor stub
+	/**
+	 * Create the panel.
+	 */
+	public OrdemDeServico() {
+		setLayout(null);
+		
+		JLabel lblEquipamento = new JLabel("Equipamento:");
+		lblEquipamento.setBounds(31, 74, 78, 14);
+		add(lblEquipamento);
+		
+		tfEquipamento = new JTextField();
+		tfEquipamento.setBounds(119, 71, 86, 20);
+		add(tfEquipamento);
+		tfEquipamento.setColumns(10);
+		
+		JLabel lblCliente = new JLabel("Cliente:");
+		lblCliente.setBounds(31, 116, 46, 14);
+		add(lblCliente);
+		
+		tfCliente = new JTextField();
+		tfCliente.setBounds(119, 113, 86, 20);
+		add(tfCliente);
+		tfCliente.setColumns(10);
+		
+		JLabel lblProblema = new JLabel("Problema:");
+		lblProblema.setBounds(31, 208, 78, 14);
+		add(lblProblema);
+		
+		JButton btnOk = new JButton("OK");
+		btnOk.setAction(actionGravarOrdem);
+		btnOk.setBounds(324, 420, 89, 23);
+		add(btnOk);
+		
+		JButton btnCancelar = new JButton("Cancelar");
+		btnCancelar.setAction(actionCancelar);
+		btnCancelar.setBounds(423, 420, 89, 23);
+		add(btnCancelar);
+		
+		JLabel lblOrdemDeServio = new JLabel("Ordem de Servi\u00E7o");
+		lblOrdemDeServio.setFont(new Font("Traditional Arabic", Font.PLAIN, 17));
+		lblOrdemDeServio.setBounds(204, 11, 139, 23);
+		add(lblOrdemDeServio);
+		
+		JTextArea taProblema = new JTextArea();
+		taProblema.setBounds(31, 243, 480, 105);
+		add(taProblema);
+
 	}
-
-
-
-	public int getCodigo() {
-		// TODO Auto-generated method stub
-		return 0;
+	private class SwingAction extends AbstractAction {
+		public SwingAction() {
+			putValue(NAME, "Gravar");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
-
-
-
-	public static OrdemDeServico create(int i, int j, String string) {
-		// TODO Auto-generated method stub
-		return null;
+	private class SwingAction_1 extends AbstractAction {
+		public SwingAction_1() {
+			putValue(NAME, "Cancelar");
+			putValue(SHORT_DESCRIPTION, "Some short description");
+			
+		}
+		public void actionPerformed(ActionEvent e) {
+		}
 	}
-	
-	
-
 }
